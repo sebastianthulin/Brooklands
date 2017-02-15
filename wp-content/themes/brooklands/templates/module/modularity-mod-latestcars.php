7d0cdea58e9b6834435888ed78d6a540e5cd838c
Sebastian Thulin <sebastian.thulin@gmail.com>
2017-02-15 20:21:36 +0100

    Added the bility to list latest cars in module.

--- wp-content/themes/brooklands/templates/module/modularity-mod-latestcars.php
+++ wp-content/themes/brooklands/templates/module/modularity-mod-latestcars.php
@@ -1,40 +1,52 @@
+<?php
+
+    //New cars controller
+    $cars = new Brooklands\Controller\Cars();
+    $cars = $cars->getCars()['car'];
+
+    //Sorting
+    if ($cars) {
+        usort($cars, function ($a, $b) {
+            return $b['id'] - $a['id'];
+        });
+    }
+
+    //Only list first three
+    $cars = array_slice($cars, 0, 3);
+
+?>
 <div class="latest-car-container">
     <div class="container">
         <div class="grid">
             <div class="grid-xs-12">
                 <?php
-                    if(!$module->hideTitle) {
+                    if (!$module->hideTitle) {
                         echo '<h2>' . $module->post_title . '</h2>';
                     }
                 ?>
             </div>
-            <div class="grid-xs-12 grid-md-3">
-                <a href="#link">
-                    <img src="http://placeholder.pics/svg/300" class="responsive" />
-                    <span class="brand">Jaguar</span>
-                    <span class="details">XE 2,0T Prestige 200HK</span>
-                    <span class="price">1 000 000:-</span>
-                </a>
-            </div>
-            <div class="grid-xs-12 grid-md-3">
-                <a href="#link">
-                    <img src="http://placeholder.pics/svg/300" class="responsive" />
-                    <span class="brand">Jaguar</span>
-                    <span class="details">XE 2,0T Prestige 200HK</span>
-                    <span class="price">1 000 000:-</span>
-                </a>
-            </div>
-            <div class="grid-xs-12 grid-md-3">
-                <a href="#link">
-                    <img src="http://placeholder.pics/svg/300" class="responsive" />
-                    <span class="brand">Jaguar</span>
-                    <span class="details">XE 2,0T Prestige 200HK</span>
-                    <span class="price">1 000 000:-</span>
-                </a>
-            </div>
+
+            <?php foreach ($cars as $car) {
+    ?>
+                <div class="grid-xs-12 grid-md-3">
+                    <a href="/vara-bilar/#modal-<?php echo $car['id'];
+    ?>">
+                        <img src="http://placeholder.pics/svg/300" class="responsive" />
+                        <span class="brand"><?php echo $car['brand'];
+    ?></span>
+                        <span class="details"><?php echo $car['modeldescription'];
+    ?></span>
+                        <span class="price"><?php echo preg_replace("/[^0-9]/", "", $car['price-sek']) ;
+    ?>:-</span>
+                    </a>
+                </div>
+            <?php
+
+} ?>
+
             <div class="grid-xs-12 grid-md-3">
                 <div class="squarebox">
-                    <?php echo apply_filters( 'the_content', $module->post_content); ?>
+                    <?php echo apply_filters('the_content', $module->post_content); ?>
                 </div>
             </div>
         </div>
