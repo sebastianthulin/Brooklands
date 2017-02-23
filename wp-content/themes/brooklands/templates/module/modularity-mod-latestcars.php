312e52e397fdd8ef4797387f9f9c8e82a496c6fe
Sebastian Thulin <sebastian.thulin@gmail.com>
2017-02-23 21:33:12 +0100

    Dynamic latest car link & menu.

--- wp-content/themes/brooklands/templates/module/modularity-mod-latestcars.php
+++ wp-content/themes/brooklands/templates/module/modularity-mod-latestcars.php
@@ -14,6 +14,16 @@
     //Only list first three
     $cars = array_slice($cars, 0, 3);
 
+    //Get link name
+    global $wpdb;
+    $link_id = $wpdb->get_col("SELECT post_id FROM {$wpdb->postmeta} WHERE meta_key = '_wp_page_template' AND meta_value = 'cars.blade.php' LIMIT 1");
+
+    if (is_array($link_id)) {
+        $permalink = get_permalink(array_pop($link_id));
+    } else {
+        $permalink = "";
+    }
+
 ?>
 <div class="latest-car-container">
     <div class="container">
@@ -26,16 +36,22 @@
                 ?>
             </div>
 
-            <?php foreach ($cars as $car) { ?>
+            <?php foreach ($cars as $car) {
+    ?>
                 <div class="grid-xs-12 grid-md-3">
-                    <a href="/vara-bilar/#modal-<?php echo $car['id']; ?>">
+                    <a href="<?php echo $permalink; ?>#modal-<?php echo $car['id'];
+    ?>">
                         <img src="http://placeholder.pics/svg/300" class="responsive" />
-                        <span class="brand"><?php echo $car['brand']; ?></span>
-                        <span class="details"><?php echo $car['modeldescription']; ?></span>
-                        <span class="price"><?php echo preg_replace("/[^0-9]/", "", $car['price-sek']); ?>:-</span>
+                        <span class="brand"><?php echo $car['brand'];
+    ?></span>
+                        <span class="details"><?php echo $car['modeldescription'];
+    ?></span>
+                        <span class="price"><?php echo preg_replace("/[^0-9]/", "", $car['price-sek']);
+    ?>:-</span>
                     </a>
                 </div>
-            <?php } ?>
+            <?php
+} ?>
 
             <div class="grid-xs-12 grid-md-3">
                 <div class="squarebox">
